408d27aa6124b1af1345337cd392f09c17dfe621
1ordezera <anderaes@gmail.com>
2023-05-05 13:04:00 -0300

    Mudanças no código

--- zipador.py
+++ zipador.py
@@ -5,7 +5,6 @@ with ZipFile("arquivo_compactado.zip", "w") as zip:
     zip.write("arquivo1.txt")  # Escrevendo o arquivo1.txt no arquivo_compactado.zip.
     zip.write("arquivo2.txt")  # Escrevendo o arquivo2.txt no arquivo_compactado.zip.
 
-
 # Extrair arquivo zip
 with ZipFile("arquivo_compactado.zip", "r") as zip:
     zip.extractall("temp")  # Extrai todos os arquivos para a pasta temp.
